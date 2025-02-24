d3475086eba9213e42ab224fa4822edd5b067398
thomasvm59 <103623857+thomasvm59@users.noreply.github.com>
2025-02-24 15:09:58 +0900

    Update database.py

--- database.py
+++ database.py
@@ -1,11 +1,16 @@
 import os
-import sqlite3
+import psycopg2
 import pandas as pd
 import logging
+from sqlalchemy import create_engine
+import urllib.parse
+import streamlit as st
 
-# Ensure the data directory exists
-DB_PATH = "housing_results.db"
-os.makedirs(os.path.dirname(DB_PATH), exist_ok=True)
+DB_NAME = st.secrets["database"]["DB_NAME"]
+DB_USER = st.secrets["database"]["DB_USER"]
+DB_PASSWORD = urllib.parse.quote(st.secrets["database"]["DB_PASSWORD"])
+DB_HOST = st.secrets["database"]["DB_HOST"]
+DB_PORT = st.secrets["database"]["DB_PORT"]
 
 # Set up logging
 logging.basicConfig(
@@ -15,29 +20,24 @@ logging.basicConfig(
 )
 
 class Database:
-    def __init__(self, db_path=DB_PATH):
-        self.db_path = db_path
-
-    def _connect(self):
-        """Create a connection to the database."""
-        return sqlite3.connect(self.db_path)
-
+    def __init__(self, db_name=DB_NAME, user=DB_USER, password=DB_PASSWORD, host=DB_HOST, port=DB_PORT):
+        self.db_url = f"postgresql://{user}:{password}@{host}:{port}/{db_name}"
+        self.engine = create_engine(self.db_url)
+    
     def save_data(self, df, table_name, if_exists="replace"):
         """Save a DataFrame to the database."""
         try:
             logging.info(f"Saving {len(df)} records to '{table_name}' table.")
-            with self._connect() as conn:
-                df.astype(str).to_sql(table_name, conn, if_exists=if_exists, index=False)
+            df.to_sql(table_name, self.engine, if_exists=if_exists, index=False)
             logging.info(f"Successfully saved {len(df)} records to '{table_name}'.")
         except Exception as e:
             logging.error(f"Error saving to '{table_name}': {e}")
-
+    
     def load_data(self, table_name):
         """Load data from a specified table."""
         try:
             logging.info(f"Loading data from '{table_name}' table.")
-            with self._connect() as conn:
-                df = pd.read_sql_query(f"SELECT * FROM {table_name}", conn)
+            df = pd.read_sql(f"SELECT * FROM {table_name}", self.engine)
             logging.info(f"Successfully loaded {len(df)} records from '{table_name}'.")
             return df
         except Exception as e:
